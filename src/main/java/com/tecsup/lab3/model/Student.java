63aa095fbe2bf3c1935af920e3e91dc192a17322
kevinsa18 <kevin.salas@tecsup.edu.pe>
2021-09-06 17:32:03 -0500

    Añadimos la clase Student y algunas instrucciones

---
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -0,0 +1,9 @@
+package com.tecsup.lab3.model;
+
+public class Student {
+  public static void main(String[] args) {
+      System.out.println("Datos del estudiante");
+      System.out.println("Kevin leonardo");
+
+  }
+}
