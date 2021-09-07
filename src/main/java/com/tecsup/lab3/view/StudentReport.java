bfff2f814181e0a0cc6da66fe1ee75d6da8bd4b0
JBCODE <73259237+PiscoJaibey@users.noreply.github.com>
2021-09-06 23:13:48 -0500

    modificamos ciertas partes

--- src/main/java/com/tecsup/lab3/view/StudentReport.java
+++ src/main/java/com/tecsup/lab3/view/StudentReport.java
@@ -3,5 +3,6 @@ package com.tecsup.lab3.view;
 public class StudentReport {
 	public static void main(String[] args) {
 		System.out.println("Generamos la clase de reportes");
+		System.out.println("Hola mi proyecto");
 	}
 }
