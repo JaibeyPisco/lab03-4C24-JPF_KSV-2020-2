b741fb9712764dd55fde69cf07c17ba562c9a778
JBCODE <73259237+PiscoJaibey@users.noreply.github.com>
2021-09-07 09:58:22 -0500

    todo esta generado automaticamente

--- src/main/java/com/tecsup/lab3/controller/StudentController.java
+++ src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -5,13 +5,11 @@ public class StudentController{
         String nombre = "Jaibey Pisco";
         int semestre = 4;
 		System.out.println("Controlador de las acciones de los estudiantes");
-
         System.out.println("Mi nombre es: " + nombre);
         System.out.println("Estoy en el semestre: " + semestre);
-
         if(nombre == "Jaibey"){
             System.out.println("Hola: " + nombre );
             System.out.println("Ya casi termino");
-        }
-        }
-	}
+        }   
+     }
+}
