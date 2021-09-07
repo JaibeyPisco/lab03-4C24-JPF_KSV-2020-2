86e55b81975ef9ee289c141b8662449a17087afa
JBCODE <73259237+PiscoJaibey@users.noreply.github.com>
2021-09-06 22:19:18 -0500

    Añadimos logica al sistema

---
+++ src/main/java/com/tecsup/lab3/controller/StudentController.java
@@ -0,0 +1,14 @@
+public class StudentController{
+    public static void main(String[] args) {
+
+
+
+
+
+        String nombre = "Jaibey Pisco";
+        String semestre = 4;
+
+        System.out.println("Mi nombre es: " + nombre);
+        System.out.println("Estoy en el semestre: " + semestre);
+    }
+}
